13396f893f882e8875e11bf93275cdea76a06609
Jai <jai.unix@gmail.com>
2016-07-17 09:44:47 +0530

    enter data manually in R studio

---
+++ enter_data_manually.R
@@ -0,0 +1,10 @@
+# enter data 
+
+x1 <- 0:10
+x2 <- 10:0
+x3 <- seq(10)
+x4 <- seq(30,0,by = -3)
+x5 <- c(5,4,1,6,7,2,2,3,2,8) # c for combine 
+x6 <- scan() # take data from keyboard 
+
+rm(list=ls())
