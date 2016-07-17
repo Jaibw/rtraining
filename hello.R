8002661c7bd32b0e27e6205013d53963ddf0aa7c
Jai <jai.unix@gmail.com>
2016-07-17 06:57:05 +0530

    hello program

---
+++ hello.R
@@ -0,0 +1,36 @@
+# this is a hello world program
+# to enable comment shift + cmd + c
+
+12+34  # basic math; press ctrl + enter
+
+1:1000# series of numbers
+
+print("Hello World") # print string 
+
+# create varlable and assign values with simple math
+a1 <- 20
+a2 <- 30
+print(a1+a2)
+
+x1 <- 1:5
+x2 <- 21:25
+print(x1+x2)
+
+# alt + - for assignment operator 
+t1 <- c(12, 30, 40, 50)
+t2 <- c(45, 56, 23, 20)
+print (t1+t2)
+
+# multiple assignment
+p1 <- p2 <- p3 <- 30
+
+# manual 
+browseURL("https://google.github.io/styleguide/Rguide.xml")
+
+# remove variables 
+rm(t1)
+rm(p1,p2,p3)
+
+# clear entires workspace
+rm(list = ls())
+
